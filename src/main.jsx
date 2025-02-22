47d50c280c6edb4308218ca74be158d5cf90f274
anikatasnin <jarintasnin09@gmail.com>
2025-02-22 21:13:47 +0600

    added Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Main from "./components/Layouts/Main";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
   },
 ]);
 
