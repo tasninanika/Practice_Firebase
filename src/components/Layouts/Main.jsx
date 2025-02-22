fe5922b925943296dd2ba1295746151fadbe97bf
anikatasnin <jarintasnin09@gmail.com>
2025-02-22 21:17:57 +0600

    updated Home.jsx

--- src/components/Layouts/Main.jsx
+++ src/components/Layouts/Main.jsx
@@ -1,7 +1,11 @@
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+
 const Main = () => {
   return (
     <div>
-      <h2></h2>
+      <Header></Header>
+      <Outlet></Outlet>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Main from "./components/Layouts/Main";
+import Home from "./components/Home/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
   },
 ]);
 
