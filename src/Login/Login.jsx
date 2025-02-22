d43477b4d653a188bfb5d4e1417fb61760d717e9
anikatasnin <jarintasnin09@gmail.com>
2025-02-22 22:45:03 +0600

    login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,12 +1,15 @@
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import auth from "../firebase/firebase.init";
+import { useState } from "react";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
-        console.log(result);
+        console.log(result.user);
+        setUser(result.user);
       })
       .catch((error) => {
         console.log(error);
@@ -16,6 +19,7 @@ const Login = () => {
   return (
     <div>
       <button onClick={handleGoogleSignIn}>Login with Google</button>
+      {user && <h4>{user.displayName}</h4>}
     </div>
   );
 };
